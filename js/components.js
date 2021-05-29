8614a941598ed8fd9178c5a969fcc5a3f4b56cc3
Alexis Aoun <pizzaman@localhost.localdomain>
2021-05-30 00:35:53 +0200

    fix: fix logo not showing in footer of home page

--- js/components.js
+++ js/components.js
@@ -84,6 +84,55 @@ class Footer extends HTMLElement {
   }
 }
 
+class FooterHome extends HTMLElement {
+  constructor() {
+    super();
+    this.innerHTML = `
+    <div id="footer">
+      <div class="col1">
+        <div class="logoTitle">
+          <img src="img/logo.png" alt="logo capa 2021" />
+          <h1>CAPA 2021</h1>
+        </div>
+        <div class="aPropos">
+          <h3>A propos</h3>
+          <p>
+            Le CAPA c'est le Comité d'Accueil des Premiers Arrivants de IMT
+            Lille Douai. Tous les membres de l'équipe sont là pour préparer ton
+            arrivée à Douai et te faire kiffer ton intégration !
+          </p>
+        </div>
+      </div>
+      <div class="col2">
+        <div class="joinUs">
+          <h3>Rejoins-nous</h3>
+          <p>
+            Sur Facebook, Instagram et YouTube tu auras accès à toutes les
+            informations utiles et tu pourras voir nos vidéos.
+          </p>
+        </div>
+        <div class="socialIcons">
+          <a href="#">
+            <i class="fa fa-instagram" aria-hidden="true"></i>
+          </a>
+          <a href="#">
+            <i class="fa fa-facebook" aria-hidden="true"></i>
+          </a>
+          <a href="#">
+            <i class="fa fa-youtube-play" aria-hidden="true"></i>
+          </a>
+          <a href="#">
+            <i class="fa fa-cloud" aria-hidden="true"></i>
+          </a>
+        </div>
+      </div>
+    </div>
+    
+    `;
+  }
+}
 customElements.define("menu-home", MenuHome);
 customElements.define("menu-page", MenuPage);
 customElements.define("pied-de-page", Footer);
+customElements.define("pied-de-page-home", FooterHome);
+
