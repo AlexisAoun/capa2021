238ecdff4e4a276c361df2cf09e45d15d40652a1
Alexis Aoun <pizzaman@localhost.localdomain>
2021-07-28 20:41:05 +0200

    styling and responsive

--- js/associations/clublist.js
+++ js/associations/clublist.js
@@ -6,11 +6,11 @@ dataObjectclub.forEach(buildNewClub);
 function buildNewClub(item, index) {
   var listItem = $('<li>' + listClubString + '</li>');
   var listItemName = $('.nameContainer', listItem);
-  listItemName.html('<a id="' + item.clubName + '"><h1>' + item.clubName + '</h1></a> <a href="#sommaire"><h1>Revenir au sommaire</h1></a>');
+  listItemName.html('<a id="' + item.clubName + '"><h1>' + item.clubName + '</h1>');
   var listItemImage = $('.imageContainer', listItem);
   listItemImage.html('<img class="club_image" src="' + item.clubImage  + '">');
   var listItemdescription = $('.descriptionContainer', listItem);
-  listItemdescription.html('<p>' + item.clubDescription + '</p>');
+  listItemdescription.html('<p>' + item.clubDescription + '<br/><a href="#sommaire" style="text-decoration-line: underline;">Revenir au sommaire</a></p>');
   $('#listClub'+item.parrent).append(listItem);
 }
 /*listItemImage.hmtl.setAttribute("src", item.clubImage);*/
@@ -23,10 +23,10 @@ dataObjectasso.forEach(buildNewAsso);
 function buildNewAsso(item, index) {
   var listItem = $(listAssoString);
   var listItemName = $('.nameContainer', listItem);
-  listItemName.html('<a id="' + item.BDX + '"><h1>' + item.assoName + '</h1></a> <a href="#sommaire"><h1>Revenir au sommaire</h1></a>');
+  listItemName.html('<a id="' + item.BDX + '"><h1>' + item.assoName + '</h1></a>');
   var listItemImage = $('.imageContainer', listItem);
   listItemImage.html('<img class="image" src="' + item.assoImage  + '">');
   var listItemdescription = $('.descriptionContainer', listItem);
-  listItemdescription.html('<p>' + item.assoDescription + '</p>');
+  listItemdescription.html('<p>' + item.assoDescription + '<br/><a href="#sommaire" style="text-decoration-line: underline;">Revenir au sommaire</a></p>');
   $('#asso'+item.BDX).append(listItem);
 }
